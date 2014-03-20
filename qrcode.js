f57b643ad5248d338769fbd2b4d23d36dcf6cb82
Sem <sembrestels@riseup.net>
2014-03-20 22:46:39 +0100

    Added support for SVG QR codes into HTML.

--- qrcode.js
+++ qrcode.js
@@ -556,6 +556,10 @@ var QRCode;
 		if (typeof el == "string") {
 			el = document.getElementById(el);
 		}
+
+		if (this._htOption.useSVG) {
+			Drawing = svgDrawer;
+		}
 		
 		this._android = _getAndroid();
 		this._el = el;
