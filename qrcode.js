540ce134666b180a632b9f772cd72acff0aef320
metacortex <9ruvie@gmail.com>
2014-10-30 19:12:16 +0900

    remove coment

--- qrcode.js
+++ qrcode.js
@@ -8,7 +8,6 @@
  * @see <a href="http://www.d-project.com/" target="_blank">http://www.d-project.com/</a>
  * @see <a href="http://jeromeetienne.github.com/jquery-qrcode/" target="_blank">http://jeromeetienne.github.com/jquery-qrcode/</a>
  */
-// var QRCode;
 
 (function (root, factory) {
 	
