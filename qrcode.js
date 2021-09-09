417e0e2e61d44ecde5421f9bf14b5fa19667bf83
xubing <xubing@yiche.com>
2021-09-09 17:34:40 +0800

    fix: Android test RegExp can't match Android 10 or 10.1 and so on

--- qrcode.js
+++ qrcode.js
@@ -162,7 +162,7 @@ var QRCode;
 		
 		if (/android/i.test(sAgent)) { // android
 			android = true;
-			var aMat = sAgent.toString().match(/android ([0-9]\.[0-9])/i);
+			var aMat = sAgent.toString().match(/android (([0-9])+(\.[0-9]+)?)/i);
 			
 			if (aMat && aMat[1]) {
 				android = parseFloat(aMat[1]);
