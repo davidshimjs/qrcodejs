7ac451c044b84317a6eb3ff9fb570d3fd7cd3fe9
19z <maitiangroup@gmail.com>
2022-05-03 11:44:36 +0800

    UTF8 support

--- qrcode.js
+++ qrcode.js
@@ -26,42 +26,27 @@ var QRCode;
 	//   http://www.denso-wave.com/qrcode/faqpatent-e.html
 	//
 	//---------------------------------------------------------------------
+	function toUTF8(text){
+		const code = encodeURIComponent(text);
+		let bytes = [];
+		for (let i = 0; i < code.length; i++) {
+			const c = code.charAt(i);
+			if (c === '%') {
+				const hex = code.charAt(i + 1) + code.charAt(i + 2);
+				const hexVal = parseInt(hex, 16);
+				bytes.push(hexVal);
+				i += 2;
+			} else {
+				bytes.push(c.charCodeAt(0));
+			}
+		}
+		return bytes;
+	}
+
 	function QR8bitByte(data) {
 		this.mode = QRMode.MODE_8BIT_BYTE;
 		this.data = data;
-		this.parsedData = [];
-
-		// Added to support UTF-8 Characters
-		for (var i = 0, l = this.data.length; i < l; i++) {
-			var byteArray = [];
-			var code = this.data.charCodeAt(i);
-
-			if (code > 0x10000) {
-				byteArray[0] = 0xF0 | ((code & 0x1C0000) >>> 18);
-				byteArray[1] = 0x80 | ((code & 0x3F000) >>> 12);
-				byteArray[2] = 0x80 | ((code & 0xFC0) >>> 6);
-				byteArray[3] = 0x80 | (code & 0x3F);
-			} else if (code > 0x800) {
-				byteArray[0] = 0xE0 | ((code & 0xF000) >>> 12);
-				byteArray[1] = 0x80 | ((code & 0xFC0) >>> 6);
-				byteArray[2] = 0x80 | (code & 0x3F);
-			} else if (code > 0x80) {
-				byteArray[0] = 0xC0 | ((code & 0x7C0) >>> 6);
-				byteArray[1] = 0x80 | (code & 0x3F);
-			} else {
-				byteArray[0] = code;
-			}
-
-			this.parsedData.push(byteArray);
-		}
-
-		this.parsedData = Array.prototype.concat.apply([], this.parsedData);
-
-		if (this.parsedData.length != this.data.length) {
-			this.parsedData.unshift(191);
-			this.parsedData.unshift(187);
-			this.parsedData.unshift(239);
-		}
+		this.parsedData = toUTF8(data)
 	}
 
 	QR8bitByte.prototype = {
@@ -503,8 +488,7 @@ var QRCode;
 	}
 
 	function _getUTF8Length(sText) {
-		var replacedText = encodeURI(sText).toString().replace(/\%[0-9a-fA-F]{2}/g, 'a');
-		return replacedText.length + (replacedText.length != sText ? 3 : 0);
+		return toUTF8(sText).length;
 	}
 	
 	/**
